1b37f04476dd130b67622196ab5aaf86ff174e8c
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2020-12-14 19:02:22 +0200

    Create exercise_1.java

---
+++ exercise_1.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = 1;
+        do {
+            System.out.print(String.format("%d ", num));
+            num++;
+        }
+        while (num <= 100);
+
+        System.out.println(" ");
+        int a, b, c, sum;
+        do {
+            System.out.println("Please enter a, b, c: ");
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            c = scanner.nextInt();
+        }
+        while ( a+b == c);
+
+        System.out.println(" ");
+        do {
+            System.out.println("Please enter dif a, b, c: ");
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            c = scanner.nextInt();
+        }
+        while ( a!=b && a!=c && b!=c);
+    }
+}
